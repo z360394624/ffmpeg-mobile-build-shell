43df8ec91fc583074f5508ed6b5e571dc621a3f0
LuciusZhang <z360394624@gmail.com>
2017-07-10 00:21:39 +0800

    modify ffmpeg build scripts for project

---
+++ ffmpeg_build4linux.sh
@@ -0,0 +1,36 @@
+. ndk.properties
+echo $NDK
+#=============for mac start=============
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew install yasm
+#brew install pkgconfig
+#=============for mac end===============
+#git clone https://github.com/FFmpeg/FFmpeg.git
+cd FFmpeg
+./configure \
+--prefix=$PREFIX_LINUX \
+--enable-small \
+--enable-gpl \
+--enable-pthreads \
+--disable-doc \
+--enable-ffmpeg \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-ffserver \
+--disable-doc \
+--disable-network \
+--disable-shared \
+--disable-encoders \
+--disable-decoders \
+--enable-protocols \
+--enable-filters \
+--enable-decoder=mpeg4 \
+--enable-decoder=h264 \
+--enable-decoder=mp3 \
+--enable-decoder=aac \
+--enable-encoder=mpeg4 \
+--enable-encoder=libx264 \
+--enable-encoder=aac \
+make clean
+make -j4
+make install
